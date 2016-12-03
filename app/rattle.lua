5021f1e54c5595e7b69033ea0f7b923409e8ebc8
bjorn <tie.372@gmail.com>
2016-12-02 21:45:05 -0800

    Start rattle;

---
+++ app/rattle.lua
@@ -0,0 +1,20 @@
+local rattle = {}
+
+function rattle:init()
+  --
+end
+
+function rattle:update(dt)
+  if self.controller then
+  end
+end
+
+function rattle:draw()
+
+end
+
+function rattle:setController(controller)
+  self.controller = controller
+end
+
+return rattle
