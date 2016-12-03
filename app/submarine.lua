7bc9be17c9aac8bee59ce98e0bf0f3885c92da53
bjorn <tie.372@gmail.com>
2016-12-03 15:44:02 -0800

    Periscope grabbing;

--- app/submarine.lua
+++ app/submarine.lua
@@ -1,4 +1,5 @@
 local submarine = {}
+local controllers = require('app/controllers')
 local vec3 = require('lib/cpml').vec3
 
 function submarine:init()
@@ -7,6 +8,21 @@ function submarine:init()
   self.periscope = {}
   self.periscope.pos = vec3(0, 2.5, -1)
   self.periscope.size = .2
+  self.periscope.grabbed = false
+end
+
+function submarine:update(dt)
+  local controller = controllers.list[1]
+  if controller then
+    local x, y, z = controller:getPosition()
+    local dist = self.periscope.pos:dist(vec3(x, y, z))
+
+    self.periscope.grabbed = dist < self.periscope.size and controller:getAxis('trigger') > .5
+
+    if self.periscope.grabbed then
+      self.periscope.pos.y = _.clamp(y, 1.5, 2.5)
+    end
+  end
 end
 
 function submarine:draw()
