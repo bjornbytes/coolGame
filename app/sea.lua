16a4e1aa408c5a2fd9357e1fa2a51c761e157f38
bjorn <tie.372@gmail.com>
2016-12-03 16:57:50 -0800

    rm skybox;

--- app/sea.lua
+++ app/sea.lua
@@ -3,15 +3,6 @@ local rattle = require 'app/rattle'
 local submarine = require 'app/submarine'
 
 function sea:init()
-  self.skybox = g.newSkybox(
-    'art/skyboxes/sea_rt.jpg',
-    'art/skyboxes/sea_lf.jpg',
-    'art/skyboxes/sea_up.jpg',
-    'art/skyboxes/sea_dn.jpg',
-    'art/skyboxes/sea_bk.jpg',
-    'art/skyboxes/sea_ft.jpg'
-  )
-
   submarine:init()
   rattle:init()
 end
@@ -19,12 +10,10 @@ end
 function sea:update(dt)
   rattle:update(dt)
   submarine:update(dt)
+  t = (t or 0) + dt
 end
 
 function sea:draw()
-  local a, rx, ry, rz = lovr.headset.getPosition()
-  self.skybox:draw(-a, rx, ry, rz)
-
   submarine:draw()
   rattle:draw()
 end
