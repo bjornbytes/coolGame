9330b07f804b66e7f5efe283e57352bd92830a72
bjorn <tie.372@gmail.com>
2016-12-03 16:01:53 -0800

    Fix lerp;

--- app/submarine.lua
+++ app/submarine.lua
@@ -19,7 +19,7 @@ function submarine:update(dt)
     local isGrabbed = dist < self.periscope.size and trigger > .8
     local targetY = isGrabbed and _.clamp(y, 1.5, 2.5) or 2.5
     local factor = (isGrabbed and 20 or 5) * dt
-    self.periscope.pos.y = _.lerp(self.periscope.pos.y, math.min(factor, 1))
+    self.periscope.pos.y = _.lerp(self.periscope.pos.y, targetY, math.min(factor, 1))
   end
 end
 
