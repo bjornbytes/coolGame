6710a3de5ce40b6ed538ab7d4abd047bdb37eb38
bjorn <tie.372@gmail.com>
2016-12-03 16:34:35 -0800

    Fix typo;

--- app/sea.lua
+++ app/sea.lua
@@ -4,12 +4,12 @@ local submarine = require 'app/submarine'
 
 function sea:init()
   self.skybox = g.newSkybox(
-    'art/sea/sea_rt.jpg',
-    'art/sea/sea_lf.jpg',
-    'art/sea/sea_up.jpg',
-    'art/sea/sea_dn.jpg',
-    'art/sea/sea_bk.jpg',
-    'art/sea/sea_ft.jpg'
+    'art/skyboxes/sea_rt.jpg',
+    'art/skyboxes/sea_lf.jpg',
+    'art/skyboxes/sea_up.jpg',
+    'art/skyboxes/sea_dn.jpg',
+    'art/skyboxes/sea_bk.jpg',
+    'art/skyboxes/sea_ft.jpg'
   )
 
   submarine:init()
